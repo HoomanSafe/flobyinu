8b579db589c893e42a4289c31806849484642782
Arc <atandarokib@gmail.com>
2023-07-27 05:43:06 +0100

    up

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <section className="py-16 w-full">
       <div className="container flex justify-center items-center">
-        <article className="flex h-auto abi flex-col items-center justify-center text-center w-full p-10">
+        <article className="flex h-auto abi flex-col items-center justify-center text-center w-full p-10 gap-12">
           <h1 className="text-center text-white text-5xl font-Bungee leading-[67.20px] tracking-wide">
             ABOUT PEGASUS
             <br />
@@ -12,15 +12,10 @@ const About = () => {
           </h1>
 
           <div className="text-center text-white text-xl font-medium leading-10">
-            Pepe and Toad the memecoin Launched on the ethereum. Our mission is
-            to provide a fun, community-driven investment opportunity that
-            celebrates the power of memes and pop culture. With a vision to
-            become the leading memecoin in the market, we believe Pepe and Toad
-            is the perfect addition to any investor’s portfolio. Join us on this
-            journey to revolutionize the world of memecoins. the market, we
-            believe Pepe and Toad is the perfect addition to any investor’s
-            portfolio. Join us on this journey to revolutionize the world of
-            memecoins.
+            Reigning in as a gift from Gods, Pegasus SniperBot aims to gives
+            users the best experience by using highly sophisticated software and
+            services. From anti rug features all the way down to auto selling
+            while you sleep has never been this easy!
           </div>
         </article>
       </div>
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -12,8 +12,10 @@ const Footer = () => {
           TODAY
         </h1>
         <p className="max-w-[599px] text-center text-white text-base font-normal leading-snug tracking-tight">
-          LootBot operates on various chains and protocols. Join today and start
-          your farming journey.
+          Reigning in as a gift from Gods, Pegasus SniperBot aims to gives users
+          the best experience by using highly sophisticated software and
+          services. From anti rug features all the way down to auto selling
+          while you sleep has never been this easy!
         </p>
         <a
           href="t.me/pegasussniper"
