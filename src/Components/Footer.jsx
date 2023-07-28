2659f1e4edcc61eb94dd0e0c0e6ca84a8e3a2ebd
Arc <atandarokib@gmail.com>
2023-07-28 19:15:37 +0100

    up

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -177,7 +177,7 @@ const Footer = () => {
         </div>
 
         <div className="text-white text-center max-w-[600px] text-xl font-normal leading-7 tracking-tight">
-          Pegasus Sniperoot {Date()} ©-All Rights Reserved.
+          Pegasus SniperBot {Date()} ©-All Rights Reserved.
         </div>
       </div>
     </footer>
