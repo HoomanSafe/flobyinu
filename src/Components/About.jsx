6a3e3e2d8f6a3fee6372f8c2732b4b226e3ae7a3
Hooman Dev <98424162+HoomanSafe@users.noreply.github.com>
2024-12-20 22:59:08 +0800

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -6,13 +6,14 @@ const About = () => {
       <div className="container flex justify-center items-center">
         <article className="flex h-auto abi flex-col items-center justify-center text-center w-full p-10 gap-12">
           <h1 className="text-center text-white text-5xl font-Bungee leading-[67.20px] tracking-wide">
-            ABOUT PEGASUS
+            ABOUT FlobyInu
             <br />
             SNIPERBOT
           </h1>
 
           <div className="text-center text-white text-xl font-medium leading-10">
-            Reigning in as a gift from the Gods, Pegasus SniperBot aims to give
+            Floby Inu is meme token on the #Binance Network, #Floby inspired by the successful journey of #Floki and aim to be his successor,
+            Floby SniperBot aims to give
             users the best experience by utilizing highly sophisticated software
             and services. From anti-rug features all the way down to
             auto-selling while you sleep, it has never been this easy!
